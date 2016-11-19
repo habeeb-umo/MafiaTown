29f0dab6363eb9001de6cfa0d734dcef3187702b
Habeeb Mohammed <habeeb.umo@gmail.com>
2016-11-18 18:39:27 -0700

    files added

---
+++ Assets/Scripts/inputText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class inputText : MonoBehaviour {
+
+    ArrayList idArray = new ArrayList();
+    ArrayList nameArray = new ArrayList();
+
+    Text playerText;
+	// Use this for initialization
+	void Start () {
+
+        playerText = GameObject.Find("InputName").GetComponent<Text>();
+
+        nameArray.Add(playerText);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
